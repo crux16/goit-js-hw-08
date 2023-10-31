e9ce6d8e7ae84e6275a0e7b5b75a47efe43d0a79
Rowell Calipay <30309887+crux16@users.noreply.github.com>
2023-10-31 15:55:29 +0800

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from "simplelightbox";
+// Additional styles import
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryUl = document.querySelector('ul.gallery');
+const photosMarkup = createGalleryItem(galleryItems);
 
-console.log(galleryItems);
+
+galleryUl.insertAdjacentHTML('beforeend',photosMarkup);
+
+function createGalleryItem(element) {
+    return element
+    .map(({ preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join('');
+};
+
+const gallery = new SimpleLightbox('.gallery a',{
+    captionsData: 'alt',
+    captionDelay: 500, // add 250ms after opening animation speed
+    captionPosition: 'bottom',
+    // close: false,
+    // showCounter: false,
+    animationSpeed: 250, //animation speed 250ms
+    hashReseted: false,
+    loop: false,
+ });
