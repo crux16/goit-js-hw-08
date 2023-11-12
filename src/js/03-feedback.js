8fd2f0fab6659e70ec50f941c4b0996dfb19976a
Rowell Calipay <30309887+crux16@users.noreply.github.com>
2023-11-13 03:44:34 +0800

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,13 @@
 import throttle from 'lodash.throttle';
 
 const formSubmit = document.querySelector('.feedback-form');
+const value = localStorage.getItem('feedback-form-state');
+if(value!==null){
+    const parseValue = JSON.parse(value);
+    const {email,message} = formSubmit;
+    email.value = parseValue.email;
+    message.value = parseValue.message;
+}
 
 const formHandler = (event) =>     {
     event.preventDefault();
